7d1e1d484fca80c10a2c99ea8952c1372e05c86e
Shuaib Parker <parkershuaib@gmail.com>
2020-06-09 02:55:40 +0200

    Initial commit

---
+++ Client/Client/src/Client_1.java
@@ -0,0 +1,11 @@
+import java.security.NoSuchAlgorithmException;
+
+public class Client_1
+{
+
+    public static void main(String[] args) throws NoSuchAlgorithmException
+    {
+        Client client=new Client("127.0.0.1");
+        client.startRunning();
+    }
+}
